b39fe4b7fdf81655312d0ceb916efe3148013b57
Jake <jakepricep99@gmail.com>
2023-01-24 14:04:15 -0600

    TaskDao, Task

--- RemindMeModule/src/main/dynamodb/TaskDao.java
+++ RemindMeModule/src/main/dynamodb/TaskDao.java
@@ -1,6 +1,8 @@
 package dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import dynamodb.models.Task;
+import exceptions.TaskNotFoundException;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -14,4 +16,12 @@ public class TaskDao {
         this.dynamoDBMapper = dynamoDBMapper;
     }
 
+    public Task getTask(String taskManagerId, String taskId) {
+        Task task = dynamoDBMapper.load(Task.class, taskManagerId, taskId);
+        if (null == task) {
+            throw new TaskNotFoundException(
+                    String.format("Could not find Task with TaskManagerId '%s' and TaskId %d"));
+        }
+        return task;
+    }
 }

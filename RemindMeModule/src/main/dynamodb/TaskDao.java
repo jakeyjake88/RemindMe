aba69650da620ec7d43b078ea63875b0708ef107
Jake <jakepricep99@gmail.com>
2023-01-24 14:20:08 -0600

    getAllTasksForManager

--- RemindMeModule/src/main/dynamodb/TaskDao.java
+++ RemindMeModule/src/main/dynamodb/TaskDao.java
@@ -1,11 +1,20 @@
 package dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import dynamodb.models.Task;
 import exceptions.TaskNotFoundException;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Singleton
 public class TaskDao {
@@ -23,5 +32,15 @@ public class TaskDao {
                     String.format("Could not find Task with TaskManagerId '%s' and TaskId %d"));
         }
         return task;
+    }
+
+    public List<Task> getAllTasksForManager(String taskManagerId) {
+        Map<String, AttributeValue> rs = new HashMap<String, AttributeValue>();
+        rs.put(":val1", new AttributeValue().withS(taskManagerId));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("taskManagerId = :val1")
+                .withExpressionAttributeValues(rs);
+        return dynamoDBMapper.scan(Task.class, scanExpression);
     }
 }

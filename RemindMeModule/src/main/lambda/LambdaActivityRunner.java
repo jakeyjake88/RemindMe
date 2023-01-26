da739e0d6eacf95bc3e9f606bbc1f9e1d3fc2eac
Jake <jakepricep99@gmail.com>
2023-01-26 09:49:52 -0600

    adding all the lambda files

---
+++ RemindMeModule/src/main/lambda/LambdaActivityRunner.java
@@ -0,0 +1,31 @@
+package lambda;
+
+import dependencies.ServiceComponent;
+
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+public class LambdaActivityRunner<TRequest, TResult> {
+    private ServiceComponent service;
+
+
+    protected LambdaResponse runActivity(
+            Supplier<TRequest> requestSupplier,
+            BiFunction<TRequest, ServiceComponent, TResult> handleRequest) {
+        try {
+            TRequest request = requestSupplier.get();
+            ServiceComponent serviceComponent = getService();
+            TResult result = handleRequest.apply(request, serviceComponent);
+            return LambdaResponse.success(result);
+        } catch (Exception e) {
+            return LambdaResponse.error(e);
+        }
+    }
+
+    private ServiceComponent getService() {
+        if (service == null) {
+            service = DaggerServiceComponent.create();
+        }
+        return service;
+    }
+}

0f5e19ab2213106a62c9f68761a28871f2a21090
Jake <jakepricep99@gmail.com>
2023-01-27 15:20:56 -0600

    GetAllTasks endpoint

--- RemindMeModule/src/main/java/com/nashss/se/remindme/activity/requests/GetAllTasksRequest.java
+++ RemindMeModule/src/main/java/com/nashss/se/remindme/activity/requests/GetAllTasksRequest.java
@@ -1,4 +1,33 @@
 package com.nashss.se.remindme.activity.requests;
 
 public class GetAllTasksRequest {
+    private final String taskManagerId;
+
+    private GetAllTasksRequest(String taskManagerId) {
+        this.taskManagerId = taskManagerId;
+    }
+
+    public String getTaskManagerId() {
+        return taskManagerId;
+    }
+
+    @Override
+    public String toString() {
+        return "GetAllTasksRequest{ " + "taskManagerId'" + taskManagerId + '\''
+                + '}';
+    }
+
+    //CHECKSTYLE:Off:Builder
+    public static Builder builder() { return new Builder(); }
+
+    public static class Builder {
+        private String taskManagerId;
+
+        public Builder withTaskManagerId(String taskManagerId) {
+            this.taskManagerId = taskManagerId;
+            return this;
+        }
+
+        public GetAllTasksRequest build() { return new GetAllTasksRequest(taskManagerId);}
+    }
 }
--- RemindMeModule/src/main/java/com/nashss/se/remindme/lambda/GetAllTasksLambda.java
+++ RemindMeModule/src/main/java/com/nashss/se/remindme/lambda/GetAllTasksLambda.java
@@ -1,4 +1,15 @@
 package com.nashss.se.remindme.lambda;
 
-public class GetAllTasksLambda {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.remindme.activity.requests.GetAllTasksRequest;
+import com.nashss.se.remindme.activity.results.GetAllTasksResult;
+
+public class GetAllTasksLambda extends LambdaActivityRunner<GetAllTasksRequest, GetAllTasksResult>
+implements RequestHandler<LambdaRequest<GetAllTasksRequest>, LambdaResponse> {
+
+    @Override
+    public LambdaResponse handleRequest(LambdaRequest<GetAllTasksRequest> input, Context context) {
+        return super.runActivity(() -> input.fromPath(path -> GetAllTasksRequest.builder()))
+    }
 }
